43b934820c8a63788246b26cffc2cbd7f8452f24
Mohammed Tohamy <125455529+mohammedtohamym@users.noreply.github.com>
2024-07-25 00:34:33 +0300

    deleted trial files

--- delete_file_emptydir.py
+++
@@ -1,7 +0,0 @@
-import os
-
-#delete file
-#os.unlink(r"D:\DesignCourse - Kickstart your UIUX Design Career 2023-6 converted\Readme.txt")
-
-#delete dir
-os.rmdir(r"D:\DesignCourse - Kickstart your UIUX Design Career 2023-6 converted\converted")
--- tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-directories = []
-# for subdir, dirs, files in os.walk(rootdir):
-#     directories.append(subdir)
-# for item in directories:
-#     converted_dir = item + r'\converted'
-#  
