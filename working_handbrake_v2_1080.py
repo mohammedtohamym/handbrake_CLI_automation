9a8f11bcb7e2658cf2544383c4e68f4e1d6556ba
Mohammed Tohamy <125455529+mohammedtohamym@users.noreply.github.com>
2024-07-24 11:40:27 +0300

    moved all code to their respective functions

--- working_handbrake_v2_1080.py
+++ working_handbrake_v2_1080.py
@@ -1,7 +1,44 @@
 import os
 import subprocess
 
-def Move_converted_file_and_delete_converted(root):
+def Create_converted_dirs(root):
+    # make a folder named converted inside all of the folders in root directorey
+    directories = []
+    for subdir, dirs, files in os.walk(root):
+        directories.append(subdir)
+    for item in directories:
+        this_is_converted_dir = item.endswith('converted')
+        converted_dir = item + r'\converted'
+        converted_dir_existes = os.path.isdir(converted_dir)
+        if not converted_dir_existes and not this_is_converted_dir:
+            os.mkdir(converted_dir)
+
+def Convert_with_handbrake_CLI(root, preset):
+    for subdir, dirs, files in os.walk(root):
+        if not subdir.endswith('converted'):
+            for file in files:
+                if file.endswith((".mp4",".mkv")):
+                    #add if statemtent here to replace mkv with mp4
+                    file_path = subdir + '\\' + file
+                    if file.endswith(".mkv"):
+                        file = file.removesuffix(".mkv")+ ".mp4"
+                    converted_path = subdir + '\\converted\\' + file
+                    
+                    #handbrake command
+                    handbrake_command = [handbrake_dir,
+                    "-i",f"{file_path}", "-o",f"{converted_path}",
+                    "--preset" ,f"{preset}" ,"--crop-mode" ,"none"]
+                    subprocess.run(handbrake_command, shell=True)
+
+def Delete_original_files(root):
+    for subdir, dirs, files in os.walk(root):
+        if not subdir.endswith('converted'):
+            for file in files:
+                if file.endswith((".mp4",".mkv")):
+                    file_path = subdir + '\\' + file
+                    os.unlink(file_path)
+
+def Move_converted_files_and_delete_converted_dirs(root):
     for subdir, dirs, files in os.walk(root):
         if subdir.endswith('converted'):
             for file in files:
@@ -20,43 +57,18 @@ handbrake_dir = r"O:\C drive\downloads\Compressed\HandBrakeCLI-1.8.1-win-x86_64\
 preset_720 = "Very Fast 720p30"
 preset_1080 = "Very Fast 1080p30"
 
-# make a folder named converted inside all of the folders in root directorey
-directories = []
-for subdir, dirs, files in os.walk(rootdir):
-    directories.append(subdir)
-for item in directories:
-    this_is_converted_dir = item.endswith('converted')
-    converted_dir = item + r'\converted'
-    converted_dir_existes = os.path.isdir(converted_dir)
-    if not converted_dir_existes and not this_is_converted_dir:
-        os.mkdir(converted_dir)
 
+
+Create_converted_dirs(rootdir)
 # perform the conversion on files only in the original folders
 # and put them in the respective 'converted' subfolder
-for subdir, dirs, files in os.walk(rootdir):
-    if not subdir.endswith('converted'):
-        for file in files:
-            if file.endswith((".mp4",".mkv")):
-                #add if statemtent here to replace mkv with mp4
-                file_path = subdir + '\\' + file
-                if file.endswith(".mkv"):
-                    file = file.removesuffix(".mkv")+ ".mp4"
-                converted_path = subdir + '\\converted\\' + file
-                
-                #handbrake command
-                handbrake_command = [handbrake_dir,
-                "-i",f"{file_path}", "-o",f"{converted_path}",
-                "--preset" ,f"{preset_720}" ,"--crop-mode" ,"none"]
-                subprocess.run(handbrake_command, shell=True)
+
+
+
+Convert_with_handbrake_CLI(rootdir, preset_1080)
 
 
 
 
-for subdir, dirs, files in os.walk(rootdir):
-    if not subdir.endswith('converted'):
-        for file in files:
-            if file.endswith((".mp4",".mkv")):
-                file_path = subdir + '\\' + file
-                os.unlink(file_path)
-
-Move_converted_file_and_delete_converted(rootdir)
+Delete_original_files(rootdir)
+Move_converted_files_and_delete_converted_dirs(rootdir)
