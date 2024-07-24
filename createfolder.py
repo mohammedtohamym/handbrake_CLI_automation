7f69081bd2113f452a13053c6c5be7e2c3797de7
Mohammed Tohamy <125455529+mohammedtohamym@users.noreply.github.com>
2024-07-24 11:52:51 +0300

    cleaned up files

--- createfolder.py
+++
@@ -1,3 +0,0 @@
-import os
-
-os.mkdir(r'O:\C drive\downloads\Compressed\HandBrakeCLI-1.8.1-win-x86_64'+r'\new')
--- make_sub_dir copy 2.py
+++
@@ -1,9 +0,0 @@
-import os
-import subprocess
-
-rootdir = r'O:\C drive\downloads\Compressed\HandBrakeCLI-1.8.1-win-x86_64'
-
-for subdir, dirs, files in os.walk(rootdir):
-    for file in files:
-        print(file)
-## join path just puts \ man XDXDXDXDXDXD
