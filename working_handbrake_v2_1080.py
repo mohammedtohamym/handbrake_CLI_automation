f4f1bf310be451abd4cfd865a09717a860242b51
Mohammed Tohamy <125455529+mohammedtohamym@users.noreply.github.com>
2024-07-25 09:30:06 +0300

    added check for the existance of 'handbrake done.txt' file

--- working_handbrake_v2_1080.py
+++ working_handbrake_v2_1080.py
@@ -1,15 +1,20 @@
 import Handbrake_CLI_Script_Functions as hpf
+import os
 
-rootdir = r"U:\placeholder"
+rootdir = r"U:\placeholder\Domestika - Paper Sculpture Techniques Create Stunning Paper Artworks 2024-7"
 handbrake_dir = r"O:\C drive\downloads\Compressed\HandBrakeCLI-1.8.1-win-x86_64\folder1\HandBrakeCLI.exe"
 
 preset_720 = "Very Fast 720p30"
 preset_1080 = "Very Fast 1080p30"
 
+if os.path.isfile(rootdir+r'\handbrake done.txt'):
+    print("Folder aleady Converted,\ndelete 'handbrake done.txt' if you wish to reconvert")
+    exit()
+
 user_input_preset = input("choos 1 for 720,\n 2 for 1080: ")
-if user_input_preset is '1':
+if user_input_preset == '1':
     preset = preset_720
-elif user_input_preset is '2':
+elif user_input_preset == '2':
     preset = preset_1080
 else:
     print("invalid input")
