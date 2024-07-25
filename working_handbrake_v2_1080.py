385215e93729eab7195eef2a2ce9449dd60505b0
Mohammed Tohamy <125455529+mohammedtohamym@users.noreply.github.com>
2024-07-25 14:51:07 +0300

    returned json file delete

--- working_handbrake_v2_1080.py
+++ working_handbrake_v2_1080.py
@@ -1,7 +1,7 @@
 import Handbrake_CLI_Script_Functions as hpf
 import os
 
-rootdir = r"U:\placeholder\Domestika - Paper Sculpture Techniques Create Stunning Paper Artworks 2024-7"
+rootdir = r"U:\placeholder\Udemy - 50 Projects In 50 Days - HTML, CSS & JavaScript 2020-11"
 handbrake_dir = r"O:\C drive\downloads\Compressed\HandBrakeCLI-1.8.1-win-x86_64\folder1\HandBrakeCLI.exe"
 
 preset_720 = "Very Fast 720p30"
@@ -27,4 +27,4 @@ hpf.Convert_with_handbrake_CLI(rootdir, handbrake_dir, preset)
 hpf.Delete_original_files(rootdir)
 hpf.Move_converted_files_and_delete_converted_dirs(rootdir)
 hpf.Mark_as_finished(rootdir)
-# hpf.Detele_convertsion_history(rootdir)
+hpf.Detele_convertsion_history(rootdir)
