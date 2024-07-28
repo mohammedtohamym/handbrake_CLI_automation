129a6a0abf88ad8f646a40f7443fbe30421cddc3
Mohammed Tohamy <125455529+mohammedtohamym@users.noreply.github.com>
2024-07-28 16:48:48 +0300

    modified path

--- working_handbrake_v2_1080.py
+++ working_handbrake_v2_1080.py
@@ -1,7 +1,7 @@
 import Handbrake_CLI_Script_Functions as hpf
 import os
 
-rootdir = r"O:\handbrake CLI\smaller"
+rootdir = r"U:\downloads\Udemy_PyQt5_From_A-Z_2020-9.part1_Downloadly.ir\Udemy - PyQt5 From A-Z 2020-9"
 handbrake_dir = r"O:\C drive\downloads\Compressed\HandBrakeCLI-1.8.1-win-x86_64\folder1\HandBrakeCLI.exe"
 
 preset_720 = "Very Fast 720p30"
